d04bca4e1571205abd19a7658bbc397102682ddb
asrulabdullah99 <srulabdullah@gmail.com>
2023-03-31 11:47:38 +0700

    update table kehadiran, jabatan dan user detail

---
+++ database/migrations/2023_03_31_043415_table_kehadiran.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kehadiran', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->date('jam_masuk');
+            $table->date('jam_keluar');
+            $table->enum('status', ['A', 'M', 'I', 'S']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kehadiran');
+    }
+};
